d5b7fbc733e0a4914eae8fb3d365278fb6953547
Luisa Aslanidis lue.aslan@gmail.com <lue.aslan@gmail.com>
2024-03-19 14:09:18 +0100

    seeded with hobbies

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Hobby.destroy_all
+
+Hobby.create!(name: "Literature")
+Hobby.create!(name: "Music")
+Hobby.create!(name: "Sports")
+Hobby.create!(name: "Theatre")
+Hobby.create!(name: "Movies")
+Hobby.create!(name: "Photography")
+Hobby.create!(name: "Comedy")
+Hobby.create!(name: "Boardgames")
+Hobby.create!(name: "Videogames")
+Hobby.create!(name: "Art")
+Hobby.create!(name: "Coding")
+Hobby.create!(name: "Crafts")
+Hobby.create!(name: "Dance")
+Hobby.create!(name: "Brewing")
+Hobby.create!(name: "Traveling")
+Hobby.create!(name: "Animals")
+Hobby.create!(name: "Food")
+Hobby.create!(name: "Party")
+Hobby.create!(name: "Nature")
