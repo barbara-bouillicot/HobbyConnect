34b3e06a65fdff17c82ed3a781ae1d02793106c3
barbara bouillicot <barbara.bouillicot@hotmail.fr>
2024-03-28 11:36:41 +0100

    changed the search for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,22 @@
 class UsersController < ApplicationController
 
+  #def index
+    #if params[:search].present?
+    #  @users = User.near(current_user.location, 10).joins(:hobbies).distinct.where(hobbies: { name: params[:search].capitalize })
+   # else
+    #  @users = User.near(current_user.location, 10).joins(:hobbies).distinct.where(hobbies: { id: current_user.hobbies.ids })
+   # end
+ #end
+
   def index
-    if params[:search].present?
-      @users = User.near(current_user.location, 10).joins(:hobbies).distinct.where(hobbies: { name: params[:search].capitalize })
-    else
-      @users = User.near(current_user.location, 10).joins(:hobbies).distinct.where(hobbies: { id: current_user.hobbies.ids })
-    end
+  if params[:search].present?
+    query = params[:search].capitalize
+
+    @users = User.near(current_user.location, 10).joins(:hobbies).where("users.username LIKE ? OR hobbies.name = ?", "%#{query}%", query).distinct
+  else
+    @users = User.near(current_user.location, 10).joins(:hobbies).where(hobbies: { id: current_user.hobbies.ids }).distinct
   end
+end
 
   def show
     @user = User.find(params[:id])
